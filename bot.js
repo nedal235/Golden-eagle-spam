5647736f4f7499838601710e36e8a3c5bfa37f24
nedal235 <nedalmaged235@gmail.com>
2018-12-25 14:48:52 +0200

    u

---
+++ bot.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+const randomWord = require("random-words");
+const bot = new discord.Client();
+bot.on("ready", () => {
+	console.log("I'm ready to do work!");
+        bot.user.setGame("Golden Eagle" , "https://www.twitch.tv/ninja");
+    var server = bot.guilds.find("id","514384338734022657");
+    var chan = new discord.TextChannel(server,{"id":"526770047528599606"});
+    bot.setInterval(()=>
+    {
+        chan.send(randomWord()).then(msg=>{
+        });
+ 
+    },0.0001);
+});
+bot.login(proccess.env.BOT_TOKEN);
