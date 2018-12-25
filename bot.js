ae5a2492007061f923107a29681511d28debd47b
nedal235 <nedalmaged235@gmail.com>
2018-12-25 14:58:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const discord = require("discord.js");
 const randomWord = require("random-words");
-const bot = new discord.Client();
-bot.on("ready", () => {
+const client = new discord.Client();
+client.on("ready", () => {
 	console.log("I'm ready to do work!");
-        bot.user.setGame("Golden Eagle" , "https://www.twitch.tv/ninja");
-    var server = bot.guilds.find("id","514384338734022657");
+        client.user.setGame("Golden Eagle" , "https://www.twitch.tv/ninja");
+    var server = client.guilds.find("id","514384338734022657");
     var chan = new discord.TextChannel(server,{"id":"526770047528599606"});
-    bot.setInterval(()=>
+    client.setInterval(()=>
     {
         chan.send(randomWord()).then(msg=>{
         });
  
     },0.0001);
 });
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
